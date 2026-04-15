523365e44ea9529015b2bb29d1bfa8c1101dd27a
harmanpreetkaur9 <harmanpreetkaur9@rrc.ca>
2026-04-14 22:42:35 -0500

    Add uniqueness validation to category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   def self.ransackable_attributes(auth_object = nil)
     ["id", "name", "created_at", "updated_at"]
   end

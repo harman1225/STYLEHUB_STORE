571da5ca96ce03418c814302fb9dafccafc5ecf0
harmanpreetkaur9 <harmanpreetkaur9@rrc.ca>
2026-04-14 22:45:42 -0500

    Add uniqueness validation to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   has_many :product_tags, dependent: :destroy
   has_many :products, through: :product_tags
 
-validates :name, presence: true
+validates :name, presence: true, uniqueness: true
 
 def self.ransackable_attributes(auth_object = nil)
   ["id", "name", "created_at", "updated_at"]

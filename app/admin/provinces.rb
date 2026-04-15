a8800d8734f779d448630db47a83897fea6be2a6
harmanpreetkaur9 <harmanpreetkaur9@rrc.ca>
2026-04-14 23:38:13 -0500

    Fix ActiveAdmin province update by permitting parameters

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -5,8 +5,7 @@ ActiveAdmin.register Province do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :gst, :pst, :hst
-  #
+  permit_params :name, :gst, :pst, :hst  #
   # or
   #
   # permit_params do
@@ -14,5 +13,5 @@ ActiveAdmin.register Province do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
